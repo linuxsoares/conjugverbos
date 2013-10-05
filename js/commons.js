e1ffb83191b0cdd1a42cbdbb42d6f9346bd42be8
linuxsoares <linux.soares@gmail.com>
2013-10-05 14:49:23 -0700

    Versão FIREFOX OS

---
+++ js/commons.js
@@ -0,0 +1,13 @@
+
+
+function getVerbos(letra){
+	document.location.href = 'lista_verbos.html?letra='+letra;
+}
+
+function home(){
+	document.location.href = 'index.html';	
+}
+
+function getConjugacoes(verbo){
+
+}
