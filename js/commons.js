4dfd11661a4f6aff2bd3055a363855bfbc4a2aa7
linuxsoares <linux.soares@gmail.com>
2013-12-24 17:17:33 -0200

    Validação do campo BUSCA

--- js/commons.js
+++ js/commons.js
@@ -15,7 +15,24 @@ function getConjugacoes(verbo){
 function getValorBusca(){
 	var verbo = $('#nome_busca').val();
 	if(verbo != ""){
-		document.location.href = 'conjugado.html?palavra='+verbo;
+		var url = 'http://itinerariosp.com.br:8080/verbosws/rest/verbos/mobile/palavra/' + verbo;
+		url = decodeURI(url);
+        $.ajax({
+            type: "GET",
+            encoding:"ISO-8859-1",
+            url: url,
+            dataType: 'json',
+            success: function(data){
+            	if(data != ''){
+            		document.location.href = 'conjugado.html?palavra='+verbo;
+            	}else {
+            		alert(verbo.toUpperCase() + ' não encontrado na Base de Dados.');
+            	}	
+            },
+            error: function(data){
+              	console.log(data);
+            }
+		});
 	}else {
 		alert("Digite Verbo para Buscar!");
 	}
